9f539c7f80d5ca056a4288ce93b3dc5cb7da87df
jinlong0813 <ahqjjl@163.com>
2017-06-14 22:37:58 +0800

    提交代码

---
+++ app/src/main/java/org/easydarwin/easyrtmp_rtsp/Config.java
@@ -0,0 +1,20 @@
+/*
+	Copyright (c) 2012-2017 EasyDarwin.ORG.  All rights reserved.
+	Github: https://github.com/EasyDarwin
+	WEChat: EasyDarwin
+	Website: http://www.easydarwin.org
+*/
+
+package org.easydarwin.easyrtmp_rtsp;
+
+/**
+ * 类Config的实现描述：
+ */
+public class Config {
+    public static final String RTSP_URL = "RTSPUrl";
+    public static final String DEFAULT_RTSP_URL = "rtsp://admin:admin@192.168.1.222/22";
+
+    public static final String SERVER_URL = "serverUrl";
+    public static final String DEFAULT_SERVER_URL = "rtmp://www.easydss.com:10085/live/stream_"+String.valueOf((int) (Math.random() * 1000000 + 100000));
+
+}
